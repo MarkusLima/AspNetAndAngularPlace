f43a48a08c79fd3137f693bcf0822980e8b6d51e
Markus Lima <Markusmak@gmail.com>
2025-05-06 09:13:15 -0300

    controller points

---
+++ AspNetAndAngularPlace.Server/Controllers/PointsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetAndAngularPlace.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PointsController : ControllerBase
+    {
+        private readonly PointService _pointService;
+
+        public PointsController(PointService pointService)
+        {
+            _pointService = pointService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReadPointsDTO>>> GetPoints([FromQuery] int skip = 0, [FromQuery] int take = 10, [FromQuery] string keyword = "")
+        {
+            try
+            {
+                var result = await _pointService.GetBooksAsync(skip, take, keyword);
+                return Ok(result);
+            }
+            catch (ExceptionsCode ex)
+            {
+                return StatusCode(ex.StatusCode, ex.Message);
+            }
+        }
+    }
+}
